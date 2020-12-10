d30489a3e79b8483ae125285595d93748e4d89f7
david pace <dpaceoffice@gmail.com>
2020-12-10 13:49:59 -0600

    Added binary search tree

--- Data Structures and Dynamic Programming/src/com/Runner.java
+++ Data Structures and Dynamic Programming/src/com/Runner.java
@@ -4,8 +4,8 @@ public class Runner {
 	
 	public static void main(String args[]) {
 		String[] values = new String[2];
-		values[0] = "horseback";
-		values[1] = "snowflake";
+		values[0] = "pioneer";
+		values[1] = "springtime";
 		LongestCommonSubsequence lc = new LongestCommonSubsequence(values[0], values[1]);
 		lc.printLCS();
 	}
